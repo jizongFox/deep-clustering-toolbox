3fde49ac718a2bea4c796315352a691142762141
JizongXPS <jizong.peng.1@etsmtl.net>
2019-04-29 13:01:07 -0400

    first commit

--- deepclustering/meters/__init__.py
+++ deepclustering/meters/__init__.py
@@ -1,6 +1,5 @@
-from .iou import  IoU
-from .dice_meter import DiceMeter
-from .metric import AggragatedMeter , ListAggregatedMeter
 from .averagemeter import AverageValueMeter
-from .kappa import KappaMetrics,Kappa2Annotator
-
+from .dice_meter import DiceMeter
+from .iou import IoU
+from .kappa import KappaMetrics, Kappa2Annotator
+from .metric import AggragatedMeter, ListAggregatedMeter
