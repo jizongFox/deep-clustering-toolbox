992900d7486540993cef07c4a7a781aaf1e13d6f
JizongXPS <jizong.peng.1@etsmtl.net>
2019-05-01 23:05:31 -0400

    add tensor-based data augmentation and mutual information loss

--- deepclustering/augment/tensor_augment.py
+++ deepclustering/augment/tensor_augment.py
@@ -1,3 +1,239 @@
 """
 Data augmentation given only the high dimensional Tensors, instead of PIL images.
 """
+import numbers
+import random
+from typing import *
+
+import numpy as np
+import torch
+from torch.nn import functional as F
+
+T = Union[np.ndarray, torch.Tensor]
+_Tensor = (np.ndarray, torch.Tensor)
+
+
+class TensorCutout(object):
+    r"""
+    This function remove a box by randomly choose one part within image Tensor
+    """
+
+    def __init__(self, min_box: int, max_box: int, pad_value: Union[int, float] = 0) -> None:
+        r"""
+        :param min_box: minimal box size
+        :param max_box: maxinmal box size
+        """
+        super().__init__()
+        self.min_box = int(min_box)
+        self.max_box = int(max_box)
+        self.pad_value = pad_value
+
+    def __call__(self, img_tensor: T) -> T:
+        assert isinstance(img_tensor, _Tensor)
+        b, c, h, w = img_tensor.shape
+        r_img_tensor = img_tensor.copy() if isinstance(img_tensor, np.ndarray) else img_tensor.clone()
+        # find left, upper, right, lower
+        box_sz = np.random.randint(self.min_box, self.max_box + 1)
+        half_box_sz = int(np.floor(box_sz / 2.))
+        x_c = np.random.randint(half_box_sz, w - half_box_sz)
+        y_c = np.random.randint(half_box_sz, h - half_box_sz)
+        box = (
+            x_c - half_box_sz,
+            y_c - half_box_sz,
+            x_c + half_box_sz,
+            y_c + half_box_sz
+        )
+        r_img_tensor[:, :, box[1]:box[3], box[0]:box[2]] = 0
+        return r_img_tensor
+
+
+class RandomCrop(object):
+    """Crop the given Tensor Image at a random location.
+
+    Args:
+        size (sequence or int): Desired output size of the crop. If size is an
+            int instead of sequence like (h, w), a square crop (size, size) is
+            made.
+        padding (int or sequence, optional): Optional padding on each border
+            of the image. Default is None, i.e no padding. If a sequence of length
+            4 is provided, it is used to pad left, top, right, bottom borders
+            respectively. If a sequence of length 2 is provided, it is used to
+            pad left/right, top/bottom borders, respectively.
+        pad_if_needed (boolean): It will pad the image if smaller than the
+            desired size to avoid raising an exception.
+        fill: Pixel fill value for constant fill. Default is 0. If a tuple of
+            length 3, it is used to fill R, G, B channels respectively.
+            This value is only used when the padding_mode is constant
+        padding_mode: Type of padding. Should be: constant, edge,
+        reflect or symmetric. Default is constant.
+
+             - constant: pads with a constant value, this value is specified with fill
+
+             - edge: pads with the last value on the edge of the image
+
+             - reflect: pads with reflection of image (without repeating the last value on the edge)
+
+                padding [1, 2, 3, 4] with 2 elements on both sides in reflect mode
+                will result in [3, 2, 1, 2, 3, 4, 3, 2]
+
+             - symmetric: pads with reflection of image (repeating the last value on the edge)
+
+                padding [1, 2, 3, 4] with 2 elements on both sides in symmetric mode
+                will result in [2, 1, 1, 2, 3, 4, 4, 3]
+
+    """
+
+    def __init__(self, size: Union[int, Tuple[int, int], List[int]],
+                 padding: Union[int, Tuple[int, int, int, int], List[int]] = None,
+                 pad_if_needed: bool = False,
+                 fill: Union[int, float] = 0,
+                 padding_mode: str = 'constant'):
+        if isinstance(size, numbers.Number):
+            self.size: Tuple[int, int] = (int(size), int(size))
+        else:
+            self.size: Tuple[int, int] = size  # type: ignore
+        self.padding = padding
+        self.pad_if_needed = pad_if_needed
+        self.fill = fill
+        self.padding_mode = padding_mode
+
+    @staticmethod
+    def get_params(img, output_size) -> Tuple[int, int, int, int]:
+        """Get parameters for ``crop`` for a random crop.
+
+        Args:
+            img (PIL Image): Image to be cropped.
+            output_size (tuple): Expected output size of the crop.
+
+        Returns:
+            tuple: params (i, j, h, w) to be passed to ``crop`` for random crop.
+        """
+        b, c, w, h = img.shape
+        tw, th = output_size
+        if w == tw and h == th:
+            return 0, 0, h, w
+
+        i = random.randint(0, h - th)
+        j = random.randint(0, w - tw)
+        return i, j, th, tw
+
+    def __call__(self, img: T) -> T:
+        """
+        Args:
+            img (PIL Image): Image to be cropped.
+
+        Returns:
+            PIL Image: Cropped image.
+        """
+        assert isinstance(img, _Tensor)
+        b, c, h, w = img.shape
+        r_img = img.copy() if isinstance(img, np.ndarray) else img.clone()
+        if self.padding is not None:
+            if isinstance(self.padding, int):
+                padding = (self.padding, self.padding)
+            else:
+                padding = self.padding
+            r_img = np.pad(r_img, pad_width=((0, 0), (0, 0), padding, padding),
+                           constant_values=self.fill, mode=self.padding_mode)
+
+        # pad the width if needed
+        if self.pad_if_needed and r_img.shape[2] < self.size[0]:
+            r_img = np.pad(r_img,
+                           pad_width=(
+                               (0, 0),
+                               (0, 0),
+                               (
+                                   int((self.size[0] - r_img.shape[2]) / 2) + 1,
+                                   int((self.size[0] - r_img.shape[2]) / 2) + 1,
+                               ),
+                               (0, 0)
+                           ),
+                           constant_values=self.fill, mode=self.padding_mode)
+            # pad the height if needed
+        if self.pad_if_needed and r_img.shape[3] < self.size[1]:
+            r_img = np.pad(r_img,
+                           pad_width=(
+                               (0, 0),
+                               (0, 0),
+                               (0, 0),
+                               (
+                                   int((self.size[1] - r_img.shape[3]) / 2) + 1,
+                                   int((self.size[1] - r_img.shape[3]) / 2) + 1
+                               )
+                           ),
+                           constant_values=self.fill, mode=self.padding_mode)
+
+        i, j, h, w = self.get_params(r_img, self.size)
+
+        return r_img[:, :, int(j):int(j + w), int(i):int(i + h)]
+
+    def __repr__(self) -> str:
+        return self.__class__.__name__ + '(size={0}, padding={1})'.format(self.size, self.padding)
+
+
+class Resize(object):
+    """Resize the input PIL Image to the given size.
+
+    Args:
+        size (sequence or int): Desired output size. If size is a sequence like
+            (h, w), output size will be matched to this. If size is an int,
+            smaller edge of the image will be matched to this number.
+            i.e, if height > width, then image will be rescaled to
+            (size * height / width, size)
+        interpolation (int, optional): Desired interpolation. Default is
+            ``PIL.Image.BILINEAR``
+    """
+
+    def __init__(self, size, interpolation):
+        assert isinstance(size, int) or (isinstance(size, Iterable) and len(size) == 2)
+        self.size = size
+        self.interpolation = interpolation
+
+    def __call__(self, img: T) -> T:
+        """
+        Args:
+            img Tensor Image: Image to be scaled.
+
+        Returns:
+            PIL Image: Rescaled image.
+        """
+        ## todo speed up the resize.
+        assert isinstance(img, _Tensor)
+        b, c, h, w = img.shape
+        torch_img = torch.Tensor(img.copy()).float() if isinstance(img, np.ndarray) else img.clone()
+        torch_img = F.upsample(torch_img, size=(self.size[0], self.size[1]), mode=self.interpolation,
+                               align_corners=True)
+        if isinstance(img, np.ndarray):
+            return torch_img.detach().numpy()
+        return torch_img
+
+
+'''
+
+class CenterCrop(object):
+    """Crops the given PIL Image at the center.
+
+    Args:
+        size (sequence or int): Desired output size of the crop. If size is an
+            int instead of sequence like (h, w), a square crop (size, size) is
+            made.
+    """
+
+    def __init__(self, size: Union[int, Tuple[int, int]]):
+        if isinstance(size, numbers.Number):
+            self.size: Tuple[int, int] = (int(size), int(size))
+        else:
+            self.size: Tuple[int, int] = size  # type:ignore
+
+    def __call__(self, img: Image.Image) -> Image.Image:
+        """
+        Args:
+            img (PIL Image): Image to be cropped.
+        Returns:
+            PIL Image: Cropped image.
+        """
+        return tf.center_crop(img, self.size)
+
+    def __repr__(self) -> str:
+        return self.__class__.__name__ + '(size={0})'.format(self.size)
+'''
---
+++ test/deepclustering/augment/test_tensorCutout.py
@@ -0,0 +1,81 @@
+from unittest import TestCase
+
+import matplotlib.pyplot as plt
+import numpy as np
+import requests
+import torch
+from PIL import Image
+
+plt.ion()
+URL = 'https://cdn1.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg'
+from deepclustering.augment.tensor_augment import TensorCutout, RandomCrop, Resize
+
+
+class TestCasewithSetUp(TestCase):
+    def setUp(self) -> None:
+        self.color_img = Image.open(requests.get(URL, stream=True).raw)
+        assert np.array(self.color_img).shape[2] == 3
+        self.grey_img = Image.fromarray(np.array(self.color_img)[:, :, 0])
+        assert np.array(self.grey_img).shape.__len__() == 2
+        self.cimg_np = np.array(self.color_img).transpose((2, 0, 1))[None]
+        assert self.cimg_np.shape.__len__() == 4
+        self.cimgs_np = np.concatenate((self.cimg_np, self.cimg_np), 0)
+        assert self.cimgs_np.shape.__len__() == 4
+        assert self.cimgs_np.shape[0] == 2
+        self.timg = torch.Tensor(self.cimg_np).float()
+        self.timgs = torch.Tensor(self.cimgs_np).float()
+
+
+class TestTensorCutout(TestCasewithSetUp):
+
+    def test1(self):
+        transform = TensorCutout(200, 500)
+
+        for _ in range(100):
+            cropped_cimg = transform(self.cimg_np)
+            cropped_cimgs = transform(self.cimgs_np)
+
+    def test_torch(self):
+        transform = TensorCutout(200, 500)
+        for _ in range(100):
+            cropped_timg = transform(self.timg)
+            cropped_timgs = transform(self.timgs)
+            # plt.imshow(cropped_timgs[0][0])
+            # plt.show()
+
+
+class TensorRandomCrop(TestCasewithSetUp):
+
+    def test_num(self):
+        cropped_size = [(100, 2000), (64, 64), (128, 256), (256, 128)]
+        for c in cropped_size:
+
+            transform = RandomCrop(size=c, padding=10, padding_mode='constant', pad_if_needed=True)
+            for _ in range(100):
+                cropped_cimg = transform(self.cimg_np)
+                assert cropped_cimg.shape[2:] == c
+
+    def test_torch(self):
+        cropped_size = [(100, 2000), (64, 64), (128, 256), (256, 128)]
+        for c in cropped_size:
+            transform = RandomCrop(size=c, padding=10, padding_mode='constant', pad_if_needed=True)
+            for _ in range(100):
+                cropped_cimg = transform(self.timg)
+                cropped_cimgs = transform(self.timgs)
+                assert cropped_cimg.shape[2:] == c
+                assert cropped_cimgs.shape[2:] == c
+
+
+class TestResize(TestCasewithSetUp):
+
+    def setUp(self) -> None:
+        super().setUp()
+
+    def test_resize(self):
+        resize_size = [(100, 200), (100, 500), (500, 100), (1000, 2000)]
+        for c in resize_size:
+            transform = Resize(size=c, interpolation='bilinear')
+            r_timg = transform(self.timgs)
+            assert r_timg.shape[2:] == c
+            r_npimg = transform(self.cimgs_np)
+            assert r_npimg.shape[2:] == c
