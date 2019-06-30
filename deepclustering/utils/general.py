602e6a65538c0a5dd613a0fe09cf10684452cd54
Jizong <jizong.peng.1@etsmtl.net>
2019-06-29 21:49:17 -0400

    add report2

--- deepclustering/utils/general.py
+++ deepclustering/utils/general.py
@@ -70,30 +70,30 @@ def set_benchmark(seed):
 # tqdm
 class tqdm_(tqdm):
     def __init__(
-            self,
-            iterable=None,
-            desc=None,
-            total=None,
-            leave=False,
-            file=None,
-            ncols=15,
-            mininterval=0.1,
-            maxinterval=10.0,
-            miniters=None,
-            ascii=None,
-            disable=False,
-            unit="it",
-            unit_scale=False,
-            dynamic_ncols=False,
-            smoothing=0.3,
-            bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [" "{rate_fmt}{postfix}]",
-            initial=0,
-            position=None,
-            postfix=None,
-            unit_divisor=1000,
-            write_bytes=None,
-            gui=False,
-            **kwargs,
+        self,
+        iterable=None,
+        desc=None,
+        total=None,
+        leave=False,
+        file=None,
+        ncols=15,
+        mininterval=0.1,
+        maxinterval=10.0,
+        miniters=None,
+        ascii=None,
+        disable=False,
+        unit="it",
+        unit_scale=False,
+        dynamic_ncols=False,
+        smoothing=0.3,
+        bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [" "{rate_fmt}{postfix}]",
+        initial=0,
+        position=None,
+        postfix=None,
+        unit_divisor=1000,
+        write_bytes=None,
+        gui=False,
+        **kwargs,
     ):
         super().__init__(
             iterable,
@@ -294,9 +294,9 @@ def dict_merge(dct: Dict[str, Any], merge_dct: Dict[str, Any], re=True):
             return
     for k, v in merge_dct.items():
         if (
-                k in dct
-                and isinstance(dct[k], dict)
-                and isinstance(merge_dct[k], collections.Mapping)
+            k in dct
+            and isinstance(dct[k], dict)
+            and isinstance(merge_dct[k], collections.Mapping)
         ):
             dict_merge(dct[k], merge_dct[k])
         else:
@@ -307,8 +307,8 @@ def dict_merge(dct: Dict[str, Any], merge_dct: Dict[str, Any], re=True):
 
 # filter a flat dictionary with a lambda function
 def dict_filter(
-        dictionary: Dict[str, np.ndarray],
-        filter_func: Callable = lambda k, v: (v != 0 and not isnan(v)),
+    dictionary: Dict[str, np.ndarray],
+    filter_func: Callable = lambda k, v: (v != 0 and not isnan(v)),
 ):
     return {k: v for k, v in dictionary.items() if filter_func(k, v)}
 
@@ -354,7 +354,7 @@ def extract_from_big_dict(big_dict, keys) -> dict:
 
 # meta function for interface
 def _register(
-        name: str, callable: Callable, alias=None, CALLABLE_DICT: dict = {}
+    name: str, callable: Callable, alias=None, CALLABLE_DICT: dict = {}
 ) -> None:
     """ Private method to register the architecture to the ARCH_CALLABLES
         :param name: A str
