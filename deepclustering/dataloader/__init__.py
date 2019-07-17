fe11e0ac6cc09e202c85def99592024df899e4cd
Jizong <jizong.peng.1@etsmtl.net>
2019-07-17 14:05:15 -0400

    add dataloader script to package

---
+++ deepclustering/dataloader/__init__.py
@@ -0,0 +1,2 @@
+from .sampler import Sampler, SequentialSampler, RandomSampler, SubsetRandomSampler, WeightedRandomSampler, BatchSampler  # noqa: F401
+from .dataloader import DataLoader  # noqa: F401
---
+++ deepclustering/dataloader/__init__.pyi
@@ -0,0 +1,3 @@
+from .sampler import Sampler as Sampler, SequentialSampler as SequentialSampler, RandomSampler as RandomSampler, \
+    SubsetRandomSampler as SubsetRandomSampler, WeightedRandomSampler as WeightedRandomSampler, BatchSampler as BatchSampler
+from .dataloader import DataLoader as DataLoader
