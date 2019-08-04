ec6060ce35d7bc9ace44d7fa9072e3bc7639768d
Jizong <jizong.peng.1@etsmtl.net>
2019-08-04 16:15:56 -0400

    enhance some scheduler

--- deepclustering/weight_scheduler/customized_scheduler.py
+++ deepclustering/weight_scheduler/customized_scheduler.py
@@ -41,10 +41,11 @@ class Scheduler(object):
 
 
 class RampScheduler(Scheduler):
-    def __init__(self, begin_epoch, max_epoch, max_value, ramp_mult=-5):
+    def __init__(self, begin_epoch=0, max_epoch=10, min_value=0.0, max_value=1.0, ramp_mult=-5.0):
         super().__init__()
         self.begin_epoch = int(begin_epoch)
         self.max_epoch = int(max_epoch)
+        self.min_value = float(min_value)
         self.max_value = float(max_value)
         self.mult = float(ramp_mult)
         self.epoch = 0
@@ -55,16 +56,16 @@ class RampScheduler(Scheduler):
     @property
     def value(self):
         return self.get_lr(
-            self.epoch, self.begin_epoch, self.max_epoch, self.max_value, self.mult
+            self.epoch, self.begin_epoch, self.max_epoch, self.min_value, self.max_value, self.mult
         )
 
     @staticmethod
-    def get_lr(epoch, begin_epoch, max_epochs, max_val, mult):
+    def get_lr(epoch, begin_epoch, max_epochs, min_val, max_val, mult):
         if epoch < begin_epoch:
-            return 0.0
+            return min_val
         elif epoch >= max_epochs:
             return max_val
-        return max_val * np.exp(
+        return min_val + (max_val - min_val) * np.exp(
             mult * (1.0 - float(epoch - begin_epoch) / (max_epochs - begin_epoch)) ** 2
         )
 
