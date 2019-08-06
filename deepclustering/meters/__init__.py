c636fa963d4380cff943d4f8a4c728027b6396bf
Jizong <jizong.peng.1@etsmtl.net>
2019-08-06 17:26:45 -0400

    add AverageWithStd Meter

--- deepclustering/meters/__init__.py
+++ deepclustering/meters/__init__.py
@@ -1,9 +1,9 @@
 from .averagemeter import AverageValueMeter
+from .cache import Cache, AveragewithStd
+from .confusionmatrix import ConfusionMatrix
 from .dicemeter import BatchDiceMeter, SliceDiceMeter
+from .hausdorff import HaussdorffDistance
+from .instance import InstanceValue
 from .iou import IoU
 from .kappa import KappaMetrics, Kappa2Annotator
-from .hausdorff import HaussdorffDistance
 from .metric import AggragatedMeter, MeterInterface
-from .confusionmatrix import ConfusionMatrix
-from .cache import Cache
-from .instance import InstanceValue
---
+++ test/deepclustering/meters/test_Averagewith_std.py
@@ -0,0 +1,51 @@
+import time
+from unittest import TestCase
+
+import torch
+import numpy as np
+
+from deepclustering.meters import AveragewithStd, MeterInterface
+from deepclustering.writer.draw_csv import DrawCSV2
+
+
+class TestDrawAverageWithSTD(TestCase):
+    """
+    This is to test the plotting of mean and std of a list of varying scalars
+    """
+
+    def setUp(self) -> None:
+        config = {"avg": AveragewithStd()}
+        self.METER = MeterInterface(config)
+        columns_to_draw = [["avg_mean", "avg_lstd", "avg_hstd"]]
+        from pathlib import Path
+        self.drawer = DrawCSV2(columns_to_draw=columns_to_draw, save_dir=Path(__file__).parent)
+
+    def _train_loop(self, data, epoch):
+        for i in data:
+            self.METER["avg"].add(i)
+
+        time.sleep(0.1)
+
+    def test_torch(self):
+        for i in range(100):
+            data = torch.randn(10, 1) / (i + 1)
+            self._train_loop(data, i)
+            self.METER.step()
+            summary = self.METER.summary()
+            self.drawer.draw(summary)
+
+    def test_numpy(self):
+        for i in range(100):
+            data = np.random.randn(10, 1) / (i + 1)
+            self._train_loop(data, i)
+            self.METER.step()
+            summary = self.METER.summary()
+            self.drawer.draw(summary)
+
+    def test_list(self):
+        for i in range(100):
+            data = (np.random.randn(10, 1) / (i + 1)).squeeze().tolist()
+            self._train_loop(data, i)
+            self.METER.step()
+            summary = self.METER.summary()
+            self.drawer.draw(summary)
