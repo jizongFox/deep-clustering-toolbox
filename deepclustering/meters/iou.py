8fdcef11f80cae88b8abfcb35daf578439f86334
jizong <jizong.peng.1@etsmtl.net>
2019-12-24 13:19:27 -0500

    remove eps for iou

--- deepclustering/meters/iou.py
+++ deepclustering/meters/iou.py
@@ -102,7 +102,7 @@ class IoU(Metric):
         acc = np.diag(hist).sum() / hist.sum()
         acc_cls = np.diag(hist) / hist.sum(axis=1)
         acc_cls = np.nanmean(acc_cls)
-        iu = (np.diag(hist) + 1e-32) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist) + 1e-32)
+        iu = np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist))
         valid = hist.sum(axis=1) > 0  # added # 横着加
         mean_iu = np.nanmean(iu[valid])  ## gt 出现过的mean_iu
         freq = hist.sum(axis=1) / hist.sum()
