6d9584898db0beb0a8c7b666a009cf550255446f
jizong <jizong.peng.1@etsmtl.net>
2020-02-18 19:44:35 -0500

    update attention unet

--- deepclustering/arch/segmentation/attention_unet.py
+++ deepclustering/arch/segmentation/attention_unet.py
@@ -38,6 +38,9 @@ class up_conv(nn.Module):
 class Attention_block(nn.Module):
     def __init__(self, F_g, F_l, F_int):
         super(Attention_block, self).__init__()
+        self._F_g = F_g
+        self._F_l = F_l
+        self._F_int = F_int
 
         self.W_g = nn.Sequential(
             nn.Conv2d(F_l, F_int, kernel_size=1, stride=1, padding=0, bias=True),
