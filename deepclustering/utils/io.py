a9c6dc620f95ea2117993b30e35c344a4e32812d
jizong <jizong.peng.1@etsmtl.net>
2020-04-12 16:59:24 -0400

    improving saving config

--- deepclustering/utils/io.py
+++ deepclustering/utils/io.py
@@ -10,6 +10,11 @@ def path2Path(path):
     return Path(path) if isinstance(path, str) else path
 
 
+def path2str(path):
+    assert isinstance(path, (Path, str)), type(path)
+    return str(path)
+
+
 def yaml_load(yaml_path: Union[Path, str], verbose=False) -> Dict[str, Any]:
     """
     load yaml file given a file string-like file path. return must be a dictionary.
@@ -18,7 +23,7 @@ def yaml_load(yaml_path: Union[Path, str], verbose=False) -> Dict[str, Any]:
     :return:
     """
     assert isinstance(yaml_path, (Path, str, PosixPath)), type(yaml_path)
-    with open(str(yaml_path), "r") as stream:
+    with open(path2str(yaml_path), "r") as stream:
         data_loaded: dict = yaml.safe_load(stream)
     if verbose:
         print(f"Loaded yaml path:{str(yaml_path)}")
@@ -26,14 +31,18 @@ def yaml_load(yaml_path: Union[Path, str], verbose=False) -> Dict[str, Any]:
     return data_loaded
 
 
-load_yaml = yaml_load
-
-
-def write_yaml(dictionary: Dict, save_dir: Union[Path, str], save_name: str) -> None:
-    if isinstance(save_dir, str):
-        save_dir = Path(save_dir)
+def write_yaml(
+    dictionary: Dict, save_dir: Union[Path, str], save_name: str, force_overwrite=True
+) -> None:
+    save_path = path2Path(save_dir) / save_name
+    if save_path.exists():
+        if force_overwrite is False:
+            save_name = (
+                save_name.split(".")[0] + "_copy" + "." + save_name.split(".")[1]
+            )
     with open(str(save_dir / save_name), "w") as outfile:  # type: ignore
         yaml.dump(dictionary, outfile, default_flow_style=False)
 
 
 yaml_write = write_yaml
+load_yaml = yaml_load
